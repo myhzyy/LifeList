14030064f4ae5334d125b62b7cc8578d091c4305
myhzy <notmyhzy@hotmail.com>
2025-09-24 08:36:35 +0100

    added routing to week page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '../../node_modules/@angular/router/router_module.d';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- todo.md
+++ todo.md
@@ -1,5 +1,14 @@
+IDEAS
+
+journey
+dont know what to do first? we'll organise them for you
 level system?
 high priorties give you more points / level
 low level task are worth 10 points
 med level are worth 15
 high level are worth 25
+you'v completed 15 tasks this week - congrats!
+
+TODO:
+
+change high priortiy
