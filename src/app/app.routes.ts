fc31fce9e8d61738138327d397705a8ae520e345
myhzy <notmyhzy@hotmail.com>
2025-09-24 12:26:52 +0100

    added journey page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { TodayComponent } from './pages/today/today';
 import { Week } from './pages/week/week';
+import { Journey } from './pages/journey/journey';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'today', pathMatch: 'full' },
   { path: 'today', component: TodayComponent },
   { path: 'week', component: Week },
+  { path: 'journey', component: Journey },
 ];
---
+++ src/app/pages/journey/journey.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Journey } from './journey';
+
+describe('Journey', () => {
+  let component: Journey;
+  let fixture: ComponentFixture<Journey>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Journey]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Journey);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
